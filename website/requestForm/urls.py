8b8bb80f1166ab0344504aae827f604f4ec90dff
Jade Macalalad <119422011+MisterLabo@users.noreply.github.com>
2025-04-01 22:21:04 +0800

    Delete website/requestForm directory

--- website/requestForm/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.requestForm_request, name="Machine Engineering Request Form"),
-]
--- website/requestForm/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import HttpResponse
-
-def requestForm_request(request):
-    return HttpResponse("This is the Machine Engineering Request Form")
