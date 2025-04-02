ef617cd63d4124c7f0b9665a71a9d40d656ae47d
Jade Macalalad <119422011+MisterLabo@users.noreply.github.com>
2025-04-02 15:45:54 +0800

    Add files via upload

--- website/website/settings.py
+++ website/website/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'forms',
     'personalProfile',
     'dashboard',
+    'aboutUs',
 
 ]
 
--- website/website/urls.py
+++ website/website/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include("forms.urls")),
     path('', include("personalProfile.urls")),
     path('', include("dashboard.urls")),
+    path('', include("aboutUs.urls")),
     
 
 ]
