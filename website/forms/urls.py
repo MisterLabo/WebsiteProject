bdd815e9b1b661dcbdd449ddb932665a327a4ee9
Jade Macalalad <nerzajade@gmail.com>
2025-04-03 21:27:36 +0800

    form changes and views

--- website/forms/urls.py
+++ website/forms/urls.py
@@ -5,5 +5,5 @@ app_name = 'forms'
 
 urlpatterns = [
     path('forms/', views.form_display, name='request'),
-
+    path('submit-request/', views.submit_request, name='submit_request'),
 ]
--- website/forms/views.py
+++ website/forms/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import permission_required
 
-# Create your views here.
-from django.shortcuts import HttpResponse
-
+# Display the form
+@permission_required('forms.can_submit_request', raise_exception=True)
 def form_display(request):
     return render(request, 'form.html')
+
+# Handle form submission
+@permission_required('forms.can_submit_request', raise_exception=True)
+def submit_request(request):
+    if request.method == "POST":
+        # Process the form data
+        messages.success(request, "Request submitted successfully!")
+        return redirect('form')  # Replace 'form' with the name of your form's URL pattern
